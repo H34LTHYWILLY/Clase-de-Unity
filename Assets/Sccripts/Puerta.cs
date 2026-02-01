1140e3bed267e1c32bc709a1d314b72401e24b5a
Veguista <pgv200080@gmail.com>
2026-02-01 18:46:03 +0100

    Puertas arregladas

--- Assets/Sccripts/Puerta.cs
+++ Assets/Sccripts/Puerta.cs
@@ -5,9 +5,34 @@ using UnityEngine;
 
 public class Puerta : ObjetoInteraccionable
 {
+    bool hasInteracted = false;
+    
+    [SerializeField] float rotacionASumar = 90;
+    [SerializeField] float velocidad = 60;
+    float cuantoHeGirado = 0;
+
     public override void Interaccionar()
     {
-        // Este es mi cambio.
-        transform.parent.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+        // Exit clause.
+        if (hasInteracted)
+            return; // Deja de ejecutar.
+
+        // Si la Exit Clause es falsa.
+        hasInteracted = true;
+    }
+
+    private void Update()
+    {
+        // Exit clause.
+        if (!hasInteracted)
+            return; // Deja de ejecutar.
+
+        if (cuantoHeGirado < rotacionASumar) // Si el loop ha ocurrido < de 18 veces.
+            return;
+
+        float rotacionExtra = velocidad * Time.deltaTime;
+        cuantoHeGirado += rotacionExtra;
+        transform.parent.rotation = 
+            Quaternion.Euler(transform.parent.rotation.eulerAngles + new Vector3(0, rotacionExtra, 0));
     }
 }
