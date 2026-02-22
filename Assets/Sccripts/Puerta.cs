029ac8d95f8610c51a8449e1f6aa13db85e848e6
Veguista <pgv200080@gmail.com>
2026-02-22 20:11:05 +0100

    Puerta arreglada

--- Assets/Sccripts/Puerta.cs
+++ Assets/Sccripts/Puerta.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Puerta : ObjetoInteraccionable
 {
-    private float interacciones = 0;
-
-    [SerializeField] float rotacionASumar = 90;
-    [SerializeField] float velocidad = 60;
+    float interacciones = 0;
     float cuantoHeGirado = 0;
+
+    [SerializeField][Range(5, 200)] float rotacionASumar = 90;
+    [SerializeField][Range(5, 200)] float velocidad = 60;
+    [SerializeField] bool abrirHaciaAtras = false;
 
     public override void Interaccionar()
     {
@@ -22,16 +23,27 @@ public class Puerta : ObjetoInteraccionable
 
     private void Update()
     {
-        // Exit clause.
-        if (interacciones > 0)
-            return; // Deja de ejecutar.
+        if (AbrirPuerta())
+        {
+            float rotacionExtra = velocidad * Time.deltaTime;
+            if (abrirHaciaAtras)
+                rotacionExtra = rotacionExtra * -1;
+            cuantoHeGirado += Mathf.Abs(rotacionExtra);
 
-        if (cuantoHeGirado < rotacionASumar) // Si el loop ha ocurrido < de 18 veces.
-            return;
+            transform.parent.rotation = Quaternion.Euler(
+                transform.parent.rotation.eulerAngles + new Vector3(0, rotacionExtra, 0));
+        }
+    }
 
-        float rotacionExtra = velocidad * Time.deltaTime;
-        cuantoHeGirado += rotacionExtra;
-        transform.parent.rotation = 
-            Quaternion.Euler(transform.parent.rotation.eulerAngles + new Vector3(0, rotacionExtra, 0));
+    protected virtual bool AbrirPuerta()
+    {
+        // Exit clause => Cuando no hayamos interaccionado (interacciones no es mayor que 0)
+        if (interacciones == 0)
+            return false; // Deja de ejecutar.
+
+        if (cuantoHeGirado >= rotacionASumar) // Si el loop ha ocurrido < de 18 veces.
+            return false;
+
+        return true;
     }
 }
